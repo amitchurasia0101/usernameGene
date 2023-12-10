35d64185cb54d69085c4cf6f31229455670f8e6f
amitchurasia0101 <145024651+amitchurasia0101@users.noreply.github.com>
2023-12-10 18:42:31 +0530

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,104 @@
+// let scores = prompt("Enter a score of a student");
+
+// if (scores >= 90 && scores <= 100) {
+//     confirm(`The score of student is ${scores} and the grade is A`)
+    
+// }
+// else if (scores >= 70 && scores <= 89){
+//     confirm(`The score of student is ${scores} and the grade is B`)
+// }
+// else if (scores >=  69 && scores  <= 69){
+//     confirm(`The score of student is ${scores} and the grade is C`)
+// }
+// else if (scores >=  50 && scores  <= 59){
+//     confirm(`The score of student is ${scores} and the grade D`)
+// }
+// else{
+//     confirm(`The score of student is ${scores} and the grade is F`)
+// }
+
+
+// let c = prompt("Enter someting which u want to print 50 times")
+
+//Starting Lecture 3
+// for(i = 0 ; i <=50 ; i++){
+//     confirm("amit")
+// }
+
+
+//  Calculate sum of 1 to 5
+// let sum = 0;
+// for (let i =0; i<=5 ; i++ ){
+//     // sum += i;
+//     console.log(`i = ${i}`)
+// }
+// console.log(`sum: ${sum}`)
+
+// while loop
+// let j = 0;
+// while (j<=5) {
+//     console.log(`j = ${j}`);
+//     j++;
+    
+// }
+// console.log(`j = ${j}`);
+
+// Do whie loops
+// let k = 1;
+// do {
+//     console.log("AMIT");
+//     k++;
+
+    
+// } while (k <= 10);
+
+// for of loop
+// let m = "pnuemonoultrascopicvolcaniovocosis";
+// let size = 0;
+
+// for (const s of m) {
+//     console.log(` s = ${s}`);
+//     size++;
+    
+// }
+// console.log(` size = ${size}`);
+
+/// For in loop
+
+// let studentDetails ={
+//     name: "amit",
+//     age: 20,
+//     Grade: "A",
+//     isPass: true,
+// };
+// for (const key in studentDetails) {
+//     console.log(`key ${key} value: ${studentDetails[key]}`);
+    
+        
+//     }
+
+    //// Practise Questions
+    //    print all even num betwen 0 - 100
+    // for (i = 0; i <=100 ; i++) {
+    //     if(i % 2 ===0){
+    //         console.log(`i = ${i}`);
+    //     }
+        
+    // }
+
+
+
+    /// PRACTISE QUESTION 2
+    //  let gameNumamber = 25;
+     
+
+    //  let userNumber = prompt("Guess the Game Nummber?");
+    //  while (userNumber != gameNumamber) {
+    //     userNumber = prompt("You entered wrong number , try again!")
+    //  }
+    //  confirm("Congratulation u entered correct number")
+    //  console.log("Congratulation u entered correct number");
+
+    let names = prompt("Enter Your Name");
+    let userName = confirm(`Hii Amit Chaurasia has designed a User Name for you @${names}${names.length}`);
+    confirm(`Please enter your name`)
